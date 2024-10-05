3ecc732e6515487a73b01cfc5a4d0af8e802f8a5
divyanshu-mahto <divyanshukm17@gmail.com>
2024-10-05 13:23:36 +0530

    Initialized spring boot project

---
+++ src/main/java/com/emailService/emailServiceSpring/EmailServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.emailService.emailServiceSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailService/emailServiceSpring/EmailServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailService.emailServiceSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
